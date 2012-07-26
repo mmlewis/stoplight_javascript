63a0bdf09fa17cfde9049b9b9b9f23d1a3ff5310
Molly <mbartlet@thoughtworks.com>
2012-07-25 20:15:04 -0500

    Molly - first stage stoplight javascript problem

---
+++ spec/stoplight.spec.js
@@ -0,0 +1,6 @@
+describe("Stoplight", function() {
+  it("should make the bottom light green", function() {
+		//Add a method to change the color of the bottom cicle to green
+		//Check that the color of the bottom circle is green
+  });
+});
