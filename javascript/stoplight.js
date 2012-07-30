08366daf226eb3b107a2344818761f7c1f7475f2
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2012-07-30 15:38:15 -0500

    working tests BUT TIMEOUT CODE IS WRONG

--- javascript/stoplight.js
+++ javascript/stoplight.js
@@ -1,15 +1,29 @@
 var Stoplight = function(init_color) {
     var stoplightObj = {
-	color : init_color
+	   color : init_color
     };
     
-    stoplightObj.setColor = function(newColor) {
-	color = newColor;
+    stoplightObj.setColor = function(element, newColor) {
+        element.css('background-color',newColor)
+	    color = newColor;
     };
     
     stoplightObj.getColor = function() {
-	return color;
+	   return color;
     };
 
     return stoplightObj;
 };
+
+$(function() {
+    //$('#green').css('background-color','green');
+    var light = Stoplight('green');
+    //while (true){
+        light.setColor($('#green'),'green');
+        setTimeout(function(){light.setColor($('#green'),'green')},4000);
+        light.setColor($('#yellow'),'yellow');
+        setTimeout(function(){light.setColor($('#yellow'),'gray')},2000);
+        light.setColor($('#red'),'red');
+        setTimeout(function(){light.setColor($('#yellow'),'gray')},4000);
+    //}
+});
--- spec/stoplight.spec.js
+++ spec/stoplight.spec.js
@@ -1,10 +1,29 @@
 describe("Stoplight", function() {
   it("should make the bottom light green", function() {
-      var stoplight = Stoplight("red");
-      stoplight.setColor("green");
+  		var fakeElement = $("<div/>");
+      var stoplight = Stoplight("Red");
+      stoplight.setColor(fakeElement,"green");
       expect(stoplight.getColor()).toBe("green");
-		//Add a method to change the color of the bottom cicle to green
-		//Check that the color of the bottom circle is green
 
+      expect(fakeElement.css('background-color')).toBe("green");
   });
+
+  it("should make the middle light yellow", function() {
+  	var fakeElement = $("<div/>");
+  	var stoplight = Stoplight("green");
+  	stoplight.setColor(fakeElement,'yellow');
+  	expect(stoplight.getColor()).toBe("yellow");
+
+  	expect(fakeElement.css('background-color')).toBe("yellow");
+  });
+
+  it("should make the top light red", function() {
+  	var fakeElement = $("<div/>");
+  	var stoplight = Stoplight("green");
+  	stoplight.setColor(fakeElement,'red');
+  	expect(stoplight.getColor()).toBe("red");
+
+  	expect(fakeElement.css('background-color')).toBe("red");
+  });
+
 });
